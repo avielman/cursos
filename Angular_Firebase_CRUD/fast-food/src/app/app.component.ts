f850cac366df093c6155aa11bab622d8e38ea857
Hansel Vielman <avielman@gmail.com>
2019-06-20 20:31:24 -0600

    5.  Modificacion del curso.

--- Angular_Firebase_CRUD/fast-food/src/app/app.component.ts
+++ Angular_Firebase_CRUD/fast-food/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import 'hammerjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fast-food';
+  title = 'Comida Rápida';
 }
--- Angular_Firebase_CRUD/fast-food/src/app/app.module.ts
+++ Angular_Firebase_CRUD/fast-food/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/*
+  import { AppRoutingModule } from './app-routing.module';
+*/
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormComponent } from './components/form/form.component';
+import { OrdersComponent } from './components/orders/orders.component';
 import { OrdersListComponent } from './components/orders/orders-list/orders-list.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -20,12 +26,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+//    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
